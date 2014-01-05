fc1c12b96b87548f29b6d73ecdd4e28717c43036
sshihata <samy.shihata@yahoo.com>
2014-01-06 01:44:30 +0200

    bluetooth

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -14,24 +14,26 @@ void loop() {
   char recvChar;
   while(1){
    
-    if(Serial.available()){//check if there's any data sent from the local serial terminal, you can add the other applications here
-      recvChar = Serial.read();
-      Serial1.print(recvChar);
-    }
+    if(Serial1.available()){//check if there's any data sent from the local serial terminal, you can add the other applications here
+      recvChar = Serial1.read();
+      Serial.print(recvChar);
+    } 
   }
 }
 
 void setupBlueToothConnection()
 { 
   Serial1.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
-  Serial.read(Serial1.print("AT+PSWD=0000\r\n")); //set the bluetooth work in slave mode
-  while(Serial1.available() > 0){
-    Serial.print(Serial1.read());
-  }
+  Serial1.print("AT+PSWD=0000\r\n"); //set the bluetooth work in slave mode
+  Serial1.print("AT+VERSION?=0000\r\n"); //set the bluetooth work in slave mode
+  char c[100];
+  int i = Serial1.readBytesUntil('\r\n', c, 100);
+  Serial.print(i);
   Serial1.print("AT+NAME=slave\r\n"); //set the bluetooth name as "SeeedBTSlave"
+  i = Serial1.readBytesUntil('\r\n', c, 100);
+  Serial.print(i);
   Serial.print("AT+PAIR=74E5,43,9C5874, 100\r\n");
   delay(2000); // This delay is required.
   Serial.println("The slave bluetooth is inquirable!");
   delay(2000); // This delay is required.
-  Serial1.flush();
 }
